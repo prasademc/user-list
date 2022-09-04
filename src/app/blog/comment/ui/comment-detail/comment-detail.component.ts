fc6b1dff25eb3933a1d3b7a8c254c544df04d9ff
chaminda prasad <chaminda@192-168-1-102.tpgi.com.au>
2022-09-04 21:31:32 +1000

    update shared module

--- src/app/blog/comment/ui/comment-detail/comment-detail.component.ts
+++ src/app/blog/comment/ui/comment-detail/comment-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 // Import interfaces
 import { Comment } from '../../domain/comment.model';
@@ -7,6 +7,7 @@ import { Comment } from '../../domain/comment.model';
 	selector: 'app-comment-detail',
 	templateUrl: './comment-detail.component.html',
 	styleUrls: ['./comment-detail.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CommentDetailComponent implements OnInit, OnChanges {
 	@Input() commentData!: Comment;
